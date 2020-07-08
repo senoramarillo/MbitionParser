277e00b365e0a72fe3d712e8dd3ffeda54122b70
andy <andy@localhost>
2020-07-08 19:04:41 +0200

    - added POM.xml

--- MbitionParser/src/LogReader.java
+++ MbitionParser/src/LogReader.java
@@ -1,12 +1,12 @@
 import model.Statistics;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+// import org.apache.commons.logging.Log;
+// import org.apache.commons.logging.LogFactory;
 import processing.LogProcessing;
 import util.FileUtils;
 
 public class LogReader {
 
-	private static Log log = LogFactory.getLog(LogReader.class);
+	// private static Log log = LogFactory.getLog(LogReader.class);
 
 	public static void main(String[] args) {
 		Statistics stats = new LogProcessing("logs").getStats();
